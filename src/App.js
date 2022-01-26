af5da86fda0e20a7a74a86465d0197c95c83449b
Shahmeer Syed <syed.meer0130@gmail.com>
2022-01-27 01:36:51 +0500

    Carousel component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 //import './App.css';
 
+import "~slick-carousel/slick/slick.css";
+import "~slick-carousel/slick/slick-theme.css";
+
 import Landing from "./screens/Landing";
 
 function App() {
   return (
     <div className="App">
-        <Landing/>
+      <Landing />
     </div>
   );
 }
